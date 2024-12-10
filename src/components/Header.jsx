b0eb299ff8e2b0626982ca3a030954a754ba6be8
Endrődi Emese <emeseendrod@gmail.com>
2024-12-10 15:22:30 +0100

    everything i already did
    header, and some config

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header(){
+    return(
+       
+            <div className="header--container">
+                <img src="public/pte-logo.png" width={"95px"} height={"95px"}/>
+                <h1>Limes</h1>
+
+                <nav>
+                    
+                </nav>
+            </div>
+       
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Layout from './components/Layout'
+
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
+
+
+function App(){
+  return(
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Layout />}>
+      </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
